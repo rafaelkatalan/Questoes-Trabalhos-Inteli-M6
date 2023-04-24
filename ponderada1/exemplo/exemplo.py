a72f920751daaea05621992c5a270917c968429a
rafaelkatalan <99191700+rafaelkatalan@users.noreply.github.com>
2023-04-24 16:07:25 -0300

    Update exemplo.py

--- ponderada1/exemplo/exemplo.py
+++ ponderada1/exemplo/exemplo.py
@@ -2,20 +2,67 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+import time
+import math
 
 
 class TurtleController(Node):
     def __init__(self):
         super().__init__('turtle_controller')
-        self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
+        self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 100)
         self.timer_ = self.create_timer(0.1, self.move_turtle)
         self.twist_msg_ = Twist()
 
     def move_turtle(self):
-        self.twist_msg_.linear.x = 1.0
-        self.twist_msg_.angular.z = 0.5
-        self.publisher_.publish(self.twist_msg_)
+        for i in range(5):
+            # Move a distância de um lado do quadrado
+            self.twist_msg_.linear.x = 2.0
+            self.twist_msg_.angular.z = 0.0
+            self.publisher_.publish(self.twist_msg_)
+            time.sleep(2)
 
+            # Vira 90 graus
+            self.twist_msg_.linear.x = 0.0
+            self.twist_msg_.angular.z = math.pi/2.5
+            self.publisher_.publish(self.twist_msg_)
+            time.sleep(2)
+        
+        self.twist_msg_.linear.x = 0.5
+        self.twist_msg_.angular.z = 0.0
+        self.publisher_.publish(self.twist_msg_)
+        time.sleep(2)
+        self.twist_msg_.linear.x = 0.0
+        self.twist_msg_.angular.z = -0.1+math.pi/2
+        self.publisher_.publish(self.twist_msg_)
+        time.sleep(2)
+        self.twist_msg_.linear.x = -2.0
+        self.twist_msg_.angular.z = 0.0
+        self.publisher_.publish(self.twist_msg_)
+        time.sleep(2)
+        self.twist_msg_.linear.x = 0.0
+        self.twist_msg_.angular.z = math.pi/2
+        self.publisher_.publish(self.twist_msg_)
+        time.sleep(2)
+        self.twist_msg_.linear.x = -1.0
+        self.twist_msg_.angular.z = 0.0
+        self.publisher_.publish(self.twist_msg_)
+        time.sleep(2)
+        self.twist_msg_.linear.x = 0.0
+        self.twist_msg_.angular.z = math.pi/2
+        self.publisher_.publish(self.twist_msg_)
+        time.sleep(2)
+        self.twist_msg_.linear.x = -2.0
+        self.twist_msg_.angular.z = 0.0
+        self.publisher_.publish(self.twist_msg_)
+        time.sleep(2)
+        self.twist_msg_.linear.x = 0.0
+        self.twist_msg_.angular.z = -0.01+math.pi/2
+        self.publisher_.publish(self.twist_msg_)
+        time.sleep(2)
+        self.twist_msg_.linear.x = -1.5
+        self.twist_msg_.angular.z = 0.0
+        self.publisher_.publish(self.twist_msg_)
+        time.sleep(2)
 
 def main(args=None):
     rclpy.init()
